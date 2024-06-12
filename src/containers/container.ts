15e23880baeff9057bb342d59593ee552abad7ec
Eduardo <dukarino@gmail.com>
2024-06-12 20:49:04 -0300

    configurando osonar-qube

--- src/containers/container.ts
+++ src/containers/container.ts
@@ -6,6 +6,9 @@ import UserService from "../services/userService";
 import StudentRepository from '../repositories/studentRepository';
 import StudentService from '../services/studentService';
 import StudentController from '../controllers/studentController';
+import MessageRepository from '../repositories/messageRepository';
+import MessageService from '../services/messageService';
+import MessageController from '../controllers/messageController';
 
 //user
 Container.set(UserRepository, new UserRepository());
@@ -18,5 +21,8 @@ Container.set(StudentService, new StudentService(Container.get(StudentRepository
 Container.set(StudentController, new StudentController(Container.get(StudentService)));
 
 //Message
+Container.set(MessageRepository, new MessageRepository());
+Container.set(MessageService, new MessageService(Container.get(MessageRepository)));
+Container.set(MessageController, new MessageController(Container.get(MessageService)));
 
 export default Container;
